5f9dc0cd8c6feb1e542f6b193e175b7af59c12db
Nagateja <teja91.valiveti@gmail.com>
2022-12-26 04:08:28 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+// React Router Import
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+
+// Components and Pages Imports
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Navbar from './Components/Navbar'
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <div className="w-full h-full ">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+        <Footer/>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App
