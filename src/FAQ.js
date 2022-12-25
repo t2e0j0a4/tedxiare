af424c2214aba371fe51875005da498beb3b1391
Nagateja <teja91.valiveti@gmail.com>
2022-12-26 04:09:06 +0530

    Dummy data entered

---
+++ src/FAQ.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        id : 1,
+        question : 'Lorem Ipsum Coder Biter Vite.',
+        answer : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel quam quia, obcaecati expedita ducimus soluta illum!'
+    },
+    {
+        id : 2,
+        question : 'Lorem Ipsum Coder Biter Vite.',
+        answer : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel quam quia, obcaecati expedita ducimus soluta illum!'
+    },
+    {
+        id : 3,
+        question : 'Lorem Ipsum Coder Biter Vite.',
+        answer : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel quam quia, obcaecati expedita ducimus soluta illum!'
+    },
+    {
+        id : 4,
+        question : 'Lorem Ipsum Coder Biter Vite.',
+        answer : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel quam quia, obcaecati expedita ducimus soluta illum!'
+    },
+    {
+        id : 5,
+        question : 'Lorem Ipsum Coder Biter Vite.',
+        answer : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel quam quia, obcaecati expedita ducimus soluta illum!'
+    },
+]
+
+export default questions;
---
+++ src/Speakers.js
@@ -0,0 +1,70 @@
+import DummyPotrait from "./Assets/DummyPotrait.jpg";
+
+const speakers = [
+    {
+        id : 1,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 2,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 3,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 4,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 5,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 6,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 7,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 8,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+]
+
+export default speakers;
