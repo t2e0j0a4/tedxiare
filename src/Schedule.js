9eedaa56be8a86de8fd8f1a526bd9b66aadd3dc3
Nagateja <teja91.valiveti@gmail.com>
2022-12-28 20:10:18 +0530

    Schedule times

---
+++ src/Schedule.js
@@ -0,0 +1,40 @@
+const schedule = [
+    {
+        id : '1',
+        session : 'Speaker 1',
+        time : '9:30 AM',
+        desc : 'Why is it hard to scale up green businesses'
+    },
+    {
+        id : '2',
+        session : 'Speaker 1',
+        time : '9:30 AM',
+        desc : 'Why is it hard to scale up green businesses and how people'
+    },
+    {
+        id : '3',
+        session : 'Speaker 1',
+        time : '9:30 AM',
+        desc : 'Why is it hard to scale up green businesses'
+    },
+    {
+        id : '4',
+        session : 'Speaker 1',
+        time : '9:30 AM',
+        desc : 'Why is it hard to scale up green businesses and how people'
+    },
+    {
+        id : '5',
+        session : 'Speaker 1',
+        time : '9:30 AM',
+        desc : 'Why is it hard to scale up green businesses'
+    },
+    {
+        id : '6',
+        session : 'Speaker 1',
+        time : '9:30 AM',
+        desc : 'Why is it hard to scale up green businesses and how people'
+    },
+];
+
+export default schedule;
