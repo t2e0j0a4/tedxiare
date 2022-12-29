8e1e10f79c833c7a3c84e598b205a4c155448bfa
Shakir Farhan <shakirfarhan123@gmail.com>
2022-12-29 23:18:30 +0530

    changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 import logo from "../Assets/tedxmainlogoedit.png";
 
 import { FaInstagram } from "react-icons/fa";
-import {MdOutlineMail} from "react-icons/md";
+import { MdOutlineMail } from "react-icons/md";
 
 const Footer = () => {
 
   const moveToTop = () => {
-    window.addEventListener('scroll', () =>{
+    window.addEventListener('scroll', () => {
       window.scrollY = 0;
       console.log(window.screenY);
     })
@@ -23,12 +23,12 @@ const Footer = () => {
 
         <div className="flex flex-col w-[90%] lg:w-[auto] items-start justify-center gap-y-5">
           <div className="logo flex flex-col items-start justify-center my-0">
-              <img src={logo} className="w-[120px]" alt="TedxIARE"/>
-                <span className='mt-0 text-[12px] font-[600] text-[#EB0028]'>x<span className='text-[#ffffff]'> = independently organized event</span> </span>
+            <img src={logo} className="w-[120px]" alt="TedxIARE" />
+            <span className='mt-0 text-[12px] font-[600] text-[#EB0028]'>x<span className='text-[#ffffff]'> = independently organized event</span> </span>
           </div>
           <div className="flex flex-row gap-x-4">
-            <a href="/" target="_blank" rel="noreferrer" className='bg-[#EB0028] p-[6px] flex justify-center items-center text-[#0B0B0B] rounded-[50%]'><FaInstagram fontSize='20px'/></a>
-            <a href="/" target="_blank" rel="noreferrer" className='bg-[#EB0028] p-[6px] flex justify-center items-center text-[#0B0B0B] rounded-[50%]'><MdOutlineMail fontSize='20px'/></a>
+            <a target="_blank" href="https://www.instagram.com/tedx_iare/" target="_blank" rel="noreferrer" className='bg-[#EB0028] p-[6px] flex justify-center items-center text-[#0B0B0B] rounded-[50%]'><FaInstagram fontSize='20px' /></a>
+            <a href="/" target="_blank" rel="noreferrer" className='bg-[#EB0028] p-[6px] flex justify-center items-center text-[#0B0B0B] rounded-[50%]'><MdOutlineMail fontSize='20px' /></a>
             {/* <a href="/" target="_blank" rel="noreferrer" className='bg-[#EB0028] p-[6px] flex justify-center items-center text-[#0B0B0B] rounded-[50%]'><FaTwitter fontSize='16px'/></a> */}
           </div>
         </div>
@@ -40,8 +40,8 @@ const Footer = () => {
         <div className="flex flex-row w-[90%] justify-start lg:w-[auto] gap-x-6 items-start lg:justify-center">
           <ul className='flex flex-col items-start justify-center gap-y-2'>
             <li className='text-[#EB0028] font-[700] font-[Poppins] text-[16px] cursor-pointer'><Link to="/about#iaresitemap">Site map</Link></li>
-            <li onClick={()=>{moveToTop()}} className='text-[#ffffff] font-[500] font-[Poppins] text-[16px]'><Link to="/">Home</Link></li>
-            <li onClick={()=>{moveToTop()}} className='text-[#ffffff] font-[500] font-[Poppins] text-[16px]'><Link to="/about">About</Link></li>
+            <li onClick={() => { moveToTop() }} className='text-[#ffffff] font-[500] font-[Poppins] text-[16px]'><Link to="/">Home</Link></li>
+            <li onClick={() => { moveToTop() }} className='text-[#ffffff] font-[500] font-[Poppins] text-[16px]'><Link to="/about">About</Link></li>
             <li className='text-[#ffffff] font-[500] font-[Poppins] text-[16px]'><a target="_blank" rel="noreferrer" href="https://docs.google.com/forms/d/1arFlz1XgGeEpTj1ykSjIoagUoSCW0ExX_Nvoz6FNhuY/viewform?edit_requested=true">Register</a></li>
           </ul>
           <ul className='flex flex-col items-start justify-center gap-y-2'>
