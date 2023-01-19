eb2cd0e4a82e771fd2aa235ac60ba0b1dbb9e2aa
Nagateja <teja91.valiveti@gmail.com>
2023-01-19 21:12:25 +0530

    speakers added

--- src/Speakers.js
+++ src/Speakers.js
@@ -3,6 +3,8 @@ import pound from "./Assets/pound kakkar.png";
 import arun from "./Assets/arun krishnamurthy.png";
 import ajay from "./Assets/Ajay.png";
 import divya from "./Assets/Divya Boppana.png"
+import sailesh from "./Assets/sailesh.png";
+import sneha from "./Assets/Sneha Tulluri.png"
 
 const speakers = [
   {
@@ -32,15 +34,31 @@ const speakers = [
     id: 4,
     name: "Ajay aka Aye Jude",
     role: "Pilot, Entrepreneur & Fashion Influencer",
-    instagram : 'https://www.instagram.com/ajayfromayejude/',
+    instagram: "https://www.instagram.com/ajayfromayejude/",
     image: ajay,
   },
   {
     id: 5,
     name: "Divya Boppana",
     role: "Digital Content Creator",
-    instagram : 'https://www.instagram.com/divyaboppana_/',
+    instagram: "https://www.instagram.com/divyaboppana_/",
     image: divya,
+  },
+  {
+    id: 6,
+    name: "Sailesh Kolanu",
+    role: "Film Director",
+    linkedin: "https://www.linkedin.com/in/sailesh-kolanu-888a4864/",
+    instagram: "https://www.instagram.com/saileshkolanu/",
+    image: sailesh,
+  },
+  {
+    id: 7,
+    name: "Sneha Tulluri",
+    role: "BTech Student",
+    linkedin: "https://www.linkedin.com/in/sneha-tulluri-06789b1a5/",
+    instagram: "https://www.instagram.com/sneha_tulluri/",
+    image: sneha,
   },
 ];
 
