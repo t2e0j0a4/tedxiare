f481857a2ad1392c38a0172477c247d772aec14b
Nagateja <teja91.valiveti@gmail.com>
2023-01-30 22:38:56 +0530

    Register Page & README Updated

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -5,7 +5,7 @@ const Register = () => {
     <div className='w-[100%] h-[100vh] bg-[#0B0B0B]'>
         <div className='flex flex-col items-center justify-center font-[Poppins] w-[100%] py-6 gap-y-4 h-[100%]'>
             <span className='text-white text-[18px] text-center'>Thanks for being here!</span>
-            <span className="text-[20px] text-white text-center w-[95%]"><span className="text-[#EB0028]">TEDx</span>IARE Event has been successfully held on <span className="text-[#EB0028]">26th January 2022.</span></span>
+            <span className="text-[20px] text-white text-center w-[95%]"><span className="text-[#EB0028]">TEDx</span>IARE Event has been successfully held on <span className="text-[#EB0028]">26th January 2023.</span></span>
         </div>
     </div>
   )
